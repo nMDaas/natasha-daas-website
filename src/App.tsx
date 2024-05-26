5af647a8a8e143fa888ae475e547ac12132548f4
nMDaas <63910563+nMDaas@users.noreply.github.com>
2024-05-26 18:26:24 +0530

    attempting preloading from App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,45 @@
-import { useState } from 'react'
+import React, { useEffect } from 'react';
 import TaskBar from "./components/TaskBar";
-import CodingPage from "./components/CodingPage";
+import CodingPage from "./components/codingPage";
 
 function App() {
+  const urls = [
+    "/natasha-daas-website/code/raytracer.jpg",
+    "/natasha-daas-website/code/productionplanner.jpg",
+    "/natasha-daas-website/code/spirograph.jpg",
+    "/natasha-daas-website/code/bakeapp.jpg",
+    "/natasha-daas-website/code/plantGen.jpg",
+    "/natasha-daas-website/code/portfolio.jpg",
+    "/natasha-daas-website/code/rb.jpg",
+    "/natasha-daas-website/code/rc.jpg",
+    "/natasha-daas-website/code/rd.jpg",
+    "/natasha-daas-website/code/bakeapp.jpg",
+    "/natasha-daas-website/code/photoeditor.jpg",
+    "/natasha-daas-website/code/re.jpg",
+    "/natasha-daas-website/code/rf.jpg",
+    "/natasha-daas-website/code/rg.jpg",
+    "/natasha-daas-website/code/rh.jpg",
+    "/natasha-daas-website/code/ri.jpg",
+    "/natasha-daas-website/code/rk.gif",
+    "/natasha-daas-website/code/rl.gif",
+    "/natasha-daas-website/code/rm.gif",
+  ]
+
+  useEffect(() => {
+    preloadImages();
+  }, []);
+
+  function preloadImages() {
+    for (let i = 0; i < urls.length; i++) {
+      console.log(urls[i])
+      preloadImage(urls[i]);
+    }
+  }
+
+  function preloadImage(url) {
+    const img = new Image();
+    img.src = url;
+  }
 
   return (
     <>
@@ -13,4 +50,4 @@ function App() {
   )
 }
 
-export default App
+export default App;

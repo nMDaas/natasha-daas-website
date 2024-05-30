9939f807b2520751d4439240b121b8c34a21a72e
nMDaas <63910563+nMDaas@users.noreply.github.com>
2024-05-30 12:06:20 +0530

    adjusted sizes of circles and border

--- src/components/dataArt.tsx
+++ src/components/dataArt.tsx
@@ -3,7 +3,6 @@ import img2 from '/art/2.jpg';
 import img3 from '/art/3.jpg';
 import img4 from '/art/4.jpg';
 import img5 from '/art/5.jpg';
-import img6 from '/art/6.jpg';
 import img7 from '/art/7.jpg';
 import img8 from '/art/8.jpg';
 import img9 from '/art/9.jpg';
@@ -18,31 +17,27 @@ import img17 from '/art/17.jpg';
 import img18 from '/art/18.jpg';
 import img19 from '/art/19.jpg';
 import img20 from '/art/20.jpg';
-import img21 from '/art/21.jpg';
 import img22 from '/art/22.jpg';
 
 export default [
-    { css: img1, height: 700, size: 400},
-    { css: img2, height: 700, size: 300},
-    { css: img3, height: 700, size: 410},
-    { css: img6, height: 700, size: 300},
-    { css: img5, height: 700, size: 410},
-    { css: img9, height: 700, size: 300},
-    { css: img17, height: 700, size: 330},
-    { css: img8, height: 700, size: 400},
-    { css: img22, height: 700, size: 400},
-    { css: img13, height: 700, size: 410},
-    { css: img11, height: 700, size: 350},
-    { css: img12, height: 700, size: 320},
-    { css: img14, height: 700, size: 300},
-    { css: img16, height: 700, size: 300},
-    { css: img18, height: 700, size: 300},
-    { css: img20, height: 700, size: 410},
-    { css: img4, height: 700, size: 410},
-    { css: img15, height: 700, size: 390},
-    { css: img7, height: 700, size: 340},
-    { css: img19, height: 700, size: 300},
-    { css: img10, height: 700, size: 330},
+    { css: img1, height: 700, size: 350},
+    { css: img2, height: 700, size: 250},
+    { css: img3, height: 700, size: 360},
+    { css: img10, height: 700, size: 250},
+    { css: img5, height: 700, size: 360},
+    { css: img9, height: 700, size: 260},
+    { css: img17, height: 700, size: 280},
+    { css: img8, height: 700, size: 350},
+    { css: img22, height: 700, size: 350},
+    { css: img13, height: 700, size: 360},
+    { css: img11, height: 700, size: 300},
+    { css: img12, height: 700, size: 270},
+    { css: img14, height: 700, size: 250},
+    { css: img16, height: 700, size: 250},
+    { css: img18, height: 700, size: 250},
+    { css: img20, height: 700, size: 360},
+    { css: img4, height: 700, size: 360},
+    { css: img15, height: 700, size: 270},
+    { css: img7, height: 700, size: 290},
+    { css: img19, height: 700, size: 250},
   ]
-  
-  // { css: img21, height: 700, size: 450}

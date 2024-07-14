a339e607923f8b4b8df623ea8ea9f25f124f13f8
nMDaas <63910563+nMDaas@users.noreply.github.com>
2024-07-13 19:10:42 -0700

    home icon update

--- src/components/TaskBar.tsx
+++ src/components/TaskBar.tsx
@@ -1,12 +1,14 @@
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { gsap } from "gsap";
 import './TaskBar.css';
-import resume from "../assets/NatashaDaasResume.pdf"
+import resume from "../assets/NatashaDaasResume.pdf";
+import home1icon from '/other/home1.png';
+import home2icon from '/other/home2.png';
 import { GitHub, MailRounded, LinkedIn } from "@mui/icons-material";
-
 
 const TaskBar = () => {
   const menuBarRef = useRef(null);
+  const [homeIcon, setHomeIcon] = useState(home1icon); // State for the home icon
 
   useEffect(() => {
     const menuBar = menuBarRef.current;
@@ -19,19 +21,36 @@ const TaskBar = () => {
   }, []);
 
   const handleScrollToElement = (event, elementId) => {
-    console.log("click")
+    console.log("click");
     event.preventDefault();
     const targetElement = document.getElementById(elementId);
-    console.log(targetElement)
+    console.log(targetElement);
     if (targetElement) {
       targetElement.scrollIntoView({ behavior: "smooth" });
     }
   };
 
+  const handleMouseEnter = () => {
+    setHomeIcon(home2icon); // Set the hover image
+  };
+
+  const handleMouseLeave = () => {
+    setHomeIcon(home1icon); // Revert to the original image
+  };
+
   return (
-    <div className="menu-bar"ref={menuBarRef}>
+    <div className="menu-bar" ref={menuBarRef}>
       <ul>
-        <li><a href="#" onClick={(event) => handleScrollToElement(event, "mount-section")}>Home</a></li>
+        <li>
+          <a 
+            href="#" 
+            onClick={(event) => handleScrollToElement(event, "mount-section")}
+            onMouseEnter={handleMouseEnter} 
+            onMouseLeave={handleMouseLeave}
+          >
+            <img src={homeIcon} alt="Home Icon" className="home-icon" />
+          </a>
+        </li>
         <li><a href="#" onClick={(event) => handleScrollToElement(event, "about-section")}>About</a></li>
         <li><a href="#" onClick={(event) => handleScrollToElement(event, "code-section")}>Code</a></li>
         <li><a href="#" onClick={(event) => handleScrollToElement(event, "anim-section")}>Animation & 3D Modeling</a></li>
@@ -39,13 +58,13 @@ const TaskBar = () => {
         <li><a href="#" onClick={(event) => handleScrollToElement(event, "contact-section")}>Contact</a></li>
         <li><a href={resume} target="_blank" rel="noopener noreferrer">Resume</a></li>
         <a href="https://github.com/nMDaas" target="_blank" rel="noopener noreferrer">
-          <GitHub fontSize="large" sx={{width: "30px", height: "30px"  }}/>
+          <GitHub fontSize="large" sx={{width: "30px", height: "30px"}} />
         </a>
         <a href="mailto:daas.n@northeastern.edu">
-          <MailRounded sx={{width: "30px", height: "30px"  }}/>
+          <MailRounded sx={{width: "30px", height: "30px"}} />
         </a>
         <a href="https://www.linkedin.com/in/natashamishradaas/" target="_blank" rel="noopener noreferrer">
-          <LinkedIn sx={{width: "30px", height: "30px"  }}/>
+          <LinkedIn sx={{width: "30px", height: "30px"}} />
         </a>
       </ul>
     </div>

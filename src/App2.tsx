65eda6b0190765d5fba5c4ee3ace8818095c4784
nMDaas <63910563+nMDaas@users.noreply.github.com>
2024-09-19 00:45:42 -0400

    testing with mobileContactPage

--- src/App2.tsx
+++ src/App2.tsx
@@ -1,4 +1,4 @@
-import React, {useEffect} from "react";
+import React, { useEffect, useState } from "react";
 import TestPage from "./components/ThreeJSComponent";
 import TaskBar from "./components/TaskBar2";
 import CodingPage from "./components/CodingPage2";
@@ -12,30 +12,62 @@ import { BrowserRouter as Router, Route, Routes, useLocation } from 'react-route
 import './App.css';
 
 const ResetScroll = () => {
-  const {pathname} = useLocation();
+  const { pathname } = useLocation();
   
   useEffect(() => {
-    window.scrollTo(0,0);      
+    window.scrollTo(0, 0);      
   }, [pathname]);
   return null;
 }
 
-
 function App() {
+  const [isMobile, setIsMobile] = useState(false);
+
+  // Function to check if the screen width indicates a mobile device
+  const checkMobile = () => {
+    setIsMobile(window.matchMedia("(max-width: 768px)").matches);
+  }
+
+  useEffect(() => {
+    checkMobile(); // Check on initial render
+    // Add an event listener to update state on screen resize
+    window.addEventListener("resize", checkMobile);
+
+    // Cleanup event listener on component unmount
+    return () => {
+      window.removeEventListener("resize", checkMobile);
+    };
+  }, []);
+
   return (
     <div>
       <Router>
-        <ResetScroll/>
+        <ResetScroll />
         <Routes>
-          <Route path="natasha-daas-website/" element={<HomePage />} />
-          <Route path="natasha-daas-website/code" element={<CodingPage />} />
-          <Route path="natasha-daas-website/art" element={<ArtPage />} />
-          <Route path="natasha-daas-website/animation" element={<AnimationPage />} />
-          <Route path="natasha-daas-website/contact" element={<ContactPage />} />
+          <Route
+            path="natasha-daas-website/"
+            element={<HomePage />}
+          />
+          <Route
+            path="natasha-daas-website/code"
+            element={<CodingPage />}
+          />
+          <Route
+            path="natasha-daas-website/art"
+            element={<ArtPage />}
+          />
+          <Route
+            path="natasha-daas-website/animation"
+            element={<AnimationPage />}
+          />
+          <Route
+            path="natasha-daas-website/contact"
+            element={isMobile ? <MobileContactPage /> : <ContactPage />}
+          />
         </Routes>
       </Router>
     </div>
-  )
+  );
 }
 
 export default App;
---
+++ src/components/ContactPageMobile.tsx
@@ -0,0 +1,46 @@
+import React from "react";
+import "./ContactPageMobile.css";
+import { GitHub, MailRounded, LinkedIn } from "@mui/icons-material";
+import TaskBar from "./TaskBar2";
+import homeIcon from "/other/home2.png";
+
+const ContactPage = () => {
+  return (
+    <div>
+      <TaskBar currentPage="Contact" />
+        <img src={homeIcon} alt="Home Icon" className="contact-icon" />
+        <div className="contact-content">
+          <div className="thankyou">thank you for visiting!</div>
+          <div className="contact_text">reach out to me @</div>
+          <div className="links">
+            <a
+              href="https://www.linkedin.com/in/natashamishradaas/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <LinkedIn sx={{ width: "45px", height: "45px" }} />
+              <span className="linkDescrip">https://www.linkedin.com/in/natashamishradaas/</span>
+            </a>
+          </div>
+          <div className="links">
+            <a href="mailto:daas.n@northeastern.edu">
+              <MailRounded sx={{ width: "45px", height: "45px" }} />
+              <span className="linkDescrip">daas.n@northeastern.edu</span>
+            </a>
+          </div>
+          <div className="links">
+              <a
+                href="https://github.com/nMDaas"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+              <GitHub fontSize="large" sx={{ width: "45px", height: "45px" }} />
+              <span className="linkDescrip">https://github.com/nMDaas</span>
+            </a>
+          </div>
+        </div>
+      </div>
+  );
+};
+
+export default ContactPage;

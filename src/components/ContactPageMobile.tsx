e275ae9a7992c608d45999d47ff3a8e782dfacf5
nMDaas <63910563+nMDaas@users.noreply.github.com>
2024-09-27 03:00:53 -0400

    updates

--- src/components/ContactPageMobile.tsx
+++ src/components/ContactPageMobile.tsx
@@ -14,26 +14,24 @@ const ContactPage = () => {
           <div className="thankyou-mobile">thank you for visiting!</div>
           <div className="contact_text-mobile">reach out to me @</div>
           <div className="links-mobile">
-          <div className="links-mobile">
             <a href="mailto:daas.n@northeastern.edu">
               <MailRounded sx={{ width: "30px", height: "30px" }} />
             </a>
-          </div>
               <a
                 href="https://github.com/nMDaas"
                 target="_blank"
                 rel="noopener noreferrer"
               >
-              <GitHub fontSize="large" sx={{ width: "30px", height: "30px" }} />
-            </a>
-          </div>
-          <a
-              href="https://www.linkedin.com/in/natashamishradaas/"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <LinkedIn sx={{ width: "30px", height: "30px" }} />
-            </a>
+                <GitHub fontSize="large" sx={{ width: "30px", height: "30px" }} />
+              </a>
+              <a
+                  href="https://www.linkedin.com/in/natashamishradaas/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <LinkedIn sx={{ width: "30px", height: "30px" }} />
+                </a>
+            </div>
         </div>
       </div>
   );

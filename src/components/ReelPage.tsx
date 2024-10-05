dc31f7bd9af0f668ce3f1b9842038eeee3c38f88
nMDaas <63910563+nMDaas@users.noreply.github.com>
2024-10-05 13:46:51 -0400

    reel update

--- src/components/ReelPage.tsx
+++ src/components/ReelPage.tsx
@@ -24,7 +24,7 @@ const ReelPage = () => {
             <iframe
                 width="900" 
                 height="500" 
-                src="https://www.youtube.com/watch?v=adUf5RUTNnU" 
+                src="https://www.youtube.com/embed/adUf5RUTNnU" 
                 title="YouTube video player"
                 frameBorder="0"
                 allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"

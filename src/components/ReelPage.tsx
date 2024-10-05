5e3e6ea428833833aeee044d76590050f79e80c0
nMDaas <63910563+nMDaas@users.noreply.github.com>
2024-10-05 13:41:40 -0400

    pushing reel content

--- src/components/ReelPage.tsx
+++ src/components/ReelPage.tsx
@@ -7,17 +7,31 @@ const ReelPage = () => {
     <div>
       <TaskBar currentPage="Reel" />
       <div className="reels">
-        <div className="reelHeading">TD/Pipeline Tools & Computer Graphics Reel</div>
-        <iframe
-            width="900" 
-            height="500" 
-            src="https://www.youtube.com/embed/1WWw9eYF3nw" 
-            title="YouTube video player"
-            frameBorder="0"
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-            allowFullScreen
-        ></iframe>
-      </div>
+        <div>
+            <div className="reelHeading">TD/Pipeline Tools & Computer Graphics Reel</div>
+            <iframe
+                width="900" 
+                height="500" 
+                src="https://www.youtube.com/embed/1WWw9eYF3nw" 
+                title="YouTube video player"
+                frameBorder="0"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                allowFullScreen
+            ></iframe>
+        </div>
+        <div>
+            <div className="reelHeading">3D Art Reel</div>
+            <iframe
+                width="900" 
+                height="500" 
+                src="https://www.youtube.com/watch?v=adUf5RUTNnU" 
+                title="YouTube video player"
+                frameBorder="0"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                allowFullScreen
+            ></iframe>
+        </div>
+        </div>
     </div>
   );
 };

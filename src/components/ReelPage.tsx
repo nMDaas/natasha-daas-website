a26a287269bf1b9862479ae08a9d6fdcfac289bc
nMDaas <63910563+nMDaas@users.noreply.github.com>
2024-10-05 12:50:36 -0400

    adding reel content

--- src/components/ReelPage.tsx
+++ src/components/ReelPage.tsx
@@ -1,11 +1,23 @@
 import React from "react";
 import TaskBar from "./TaskBar2";
+import "./ReelPage.css"
 
 const ReelPage = () => {
   return (
     <div>
       <TaskBar currentPage="Reel" />
-      
+      <div className="reels">
+        <div className="reelHeading">TD/Pipeline Tools & Computer Graphics Reel</div>
+        <iframe
+            width="900" 
+            height="500" 
+            src="https://www.youtube.com/embed/1WWw9eYF3nw" 
+            title="YouTube video player"
+            frameBorder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowFullScreen
+        ></iframe>
+      </div>
     </div>
   );
 };

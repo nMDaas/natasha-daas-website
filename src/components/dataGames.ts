3a03b7488625fa2059970458f532ac4ccd1c674b
nMDaas <63910563+nMDaas@users.noreply.github.com>
2024-11-20 21:14:38 -0500

    added lilypad parkour to games section

--- src/components/dataGames.ts
+++ src/components/dataGames.ts
@@ -1,4 +1,5 @@
 import psyduck from '/games/PsyduckHuntPreview.mp4';
+import lilypad from '/games/LilypadParkour.mp4';
 
 export default [
   { blank: psyduck,
@@ -10,12 +11,29 @@ export default [
      summary: "3D Game Made In Unity",
      linkInfo: "View Demo",
      details: [
-        "Implemented game mechanics, character controllers, and animator controllers using C# scripts in Unity",
+        "Implemented game mechanics, character animations, and character interactions in Unity",
         "Scripted a custom toon shader in Unity for cartoon cell shading",
-        "Animated characters and character interactions using 3D Math and Linear Algebra, including dot product and cross product",
+        "Animated characters and character interactions using 3D Math and Linear Algebra",
         "Rigged characters and assets using Blender and Mixamo",
         "Managed NPC navigations using NavMesh and camera movements using CineMachine",
-        "Sculpted and textured terrain in Unity while importing assets from Polytope Studio and Free3D",
+        //"Sculpted and textured terrain in Unity while importing assets from Polytope Studio and Free3D",
      ],
-    }
+    },
+    {   blank: lilypad,
+        css: lilypad, 
+        hoverImages: [lilypad], 
+        height: 1730, 
+        description: "Lilypad Parkour",
+        skills: "Unity | C# | Blender", 
+        link:"https://youtu.be/ieZEHAfgGZY",
+        summary: "3D Parkour Game Built in Unity",
+        linkInfo: "View Demo",
+        details: [ 
+            "Collaborated with a team to implement game mechanics, character controllers, and animator controllers using C# scripts in Unity",
+            "Responsible for game concept, inspired by Subway Surfers, Crossy Road and Pocket Frogs",
+            "Oversaw the design and creation of game environment, including scripting for moving water",
+            "Implemented lilypad spawning and particle system algorithms for game interactions using C#",
+            "Character animations and interactions implemented using vector math and linear algebra, including dot product, cross product and ray casting"
+        ],
+        }
 ]

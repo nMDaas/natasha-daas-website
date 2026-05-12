c8ece0f8d291014289bedc1099bcaf918936372f
nMDaas <63910563+nMDaas@users.noreply.github.com>
2026-05-12 22:08:47 +0530

    Updated main2 to main and App2 to App, cleaned up unused files

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,76 @@
+import React, { useEffect, useState } from "react";
 import TestPage from "./components/ThreeJSComponent";
 import TaskBar from "./components/TaskBar";
 import CodingPage from "./components/CodingPage";
-import AnimationPage from "./components/AnimationPage";
+import AnimationPage from "./components/AnimationPage2";
 import HomePage from "./components/Home";
-import AboutPage from "./components/About";
-import ArtPage from "./components/ArtPage";
-import ContactPage from "./components/ContactPageMobile";
+import ReelPage from "./components/ReelPage";
+import ArtPage from "./components/ArtPage2";
+import ContactPage from "./components/ContactPage2";
+import ContactPageMobile from "./components/ContactPageMobile";
+import { BrowserRouter as Router, Route, Routes, useLocation } from 'react-router-dom';
 
 import './App.css';
 
+const ResetScroll = () => {
+  const { pathname } = useLocation();
+  
+  useEffect(() => {
+    window.scrollTo(0, 0);      
+  }, [pathname]);
+  return null;
+}
+
 function App() {
+  const [isMobile, setIsMobile] = useState(false);
+
+  // Function to check if the screen width indicates a mobile device
+  const checkMobile = () => {
+    setIsMobile(window.matchMedia("(max-width: 768px)").matches);
+  }
+
+  useEffect(() => {
+    checkMobile(); // Check on initial render
+    // Add an event listener to update state on screen resize
+    window.addEventListener("resize", checkMobile);
+
+    // Cleanup event listener on component unmount
+    return () => {
+      window.removeEventListener("resize", checkMobile);
+    };
+  }, []);
+
   return (
     <div>
-      <div id="taskbar">
-        <TaskBar />
-      </div>
-      <div id="mount-section">
-        <TestPage />
-      </div>
-      <div id="about-section">
-        <HomePage />
-      </div>
-      <h2 id="code-section">Coding Projects</h2>
-      <CodingPage />
-      <h2 id="anim-section">Animation & Modeling</h2>
-      <div className="animpage">
-        <AnimationPage />
-      </div>
-      <h2 id="art-section">Some Art!</h2>
-      <div className="animpage">
-        <ArtPage />
-      </div>
-      <div id="contact-section">
-        <ContactPage />
-      </div>
+      <Router>
+        <ResetScroll />
+        <Routes>
+          <Route
+            path="/"
+            element={<HomePage />}
+          />
+          <Route
+            path="/reel"
+            element={<ReelPage />}
+          />
+          <Route
+            path="/code"
+            element={<CodingPage />}
+          />
+          <Route
+            path="/art"
+            element={<ArtPage />}
+          />
+          <Route
+            path="/animation"
+            element={<AnimationPage />}
+          />
+          <Route
+            path="/contact"
+            element={isMobile ? <ContactPageMobile /> : <ContactPage />}
+          />
+        </Routes>
+      </Router>
     </div>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
-import App2 from './App2.tsx'
+import { HelmetProvider } from 'react-helmet-async';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <HelmetProvider>
+      <App />
+    </HelmetProvider>
   </React.StrictMode>,
 )
